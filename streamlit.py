aaafec20d45a3481439eb2d23ecfd7469b04e743
shar0821 <70147957+shar0821@users.noreply.github.com>
2025-02-16 23:30:36 -0800

    Third Commit

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title("Hello World") 
+
+def find_image_type(image) -> str:
+    #predict the type of item being captured
+    #return the type of item
+    #return type
+    pass
+    return ""
+img = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+def create_caption(type:str,proficiency:str) -> str:
+    #create the caption
+    #return the caption
+    pass    
+
+st.write(img)
